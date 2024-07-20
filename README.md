f84dc2d5d4de5373f3e5c5fc128c1574f527306f
Artur Mostowski <artur.mostowski@protonmail.com>
2024-07-20 14:15:02 +0200

    script 📜

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # tweet-2-bibtex
+
+## Usage
+
+`ruby main.rb <tweet-url>`
+
+e.g.
+
+`ruby main.rb https://x.com/GretaThunberg/status/1608056944501178368`
+
+## Requirements
+
+ruby and watir
+
---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'watir'
+require 'pry'
+browser = Watir::Browser.new
+
+link = ARGV[0]
+browser.goto link
+tweet = browser.article({"data-testid" =>  "tweet"})
+author = tweet.div({"data-testid" =>  "User-Name"}).text.split("\n")
+text = tweet.div({"data-testid" =>  "tweetText"}).text
+date = tweet.times.last.datetime
+date_parsed = Date.parse(date)
+
+browser.close
+
+result = "@misc{placeholder_name,
+  author = {#{author[0]} - #{author[1]}},
+  title = {#{text}},
+  year = {#{date_parsed.year}},
+  month = {#{date_parsed.month}},
+  day = {#{date_parsed.day}},
+  howpublished = {\\url{#{link.gsub(/x\.com/, 'twitter.com')}}},
+  note = {Accessed: #{Date.today.iso8601}}
+}"
+puts result
